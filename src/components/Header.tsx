60ff4c46103276a6b4aeee3ff486037ed2f562da
anondevmeister <degenprogrammer@gmail.com>
2024-01-05 14:30:31 +0100

    pause tg button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,49 +6,46 @@ const Header = () => {
   return (
     <header className="flex h-[10%] items-center justify-between bg-turquoise p-4 font-semibold md:px-8">
       <div className="flex items-center gap-2">
-      <Link href={'/'}>
-        <div className="flex items-center gap-2">
-          <h1>PEPSEI</h1>
-        
-          <Image
-            width={32}
-            height={32}
-            alt={"logo"}
-            src={"/pepeplaceholder.png"}
-            className="rounded-full cursor-pointer active:opacity-80"
-          />
-        </div>
+        <Link href={"/"}>
+          <div className="flex items-center gap-2">
+            <h1>PEPSEI</h1>
+
+            <Image
+              width={32}
+              height={32}
+              alt={"logo"}
+              src={"/pepeplaceholder.png"}
+              className="cursor-pointer rounded-full active:opacity-80"
+            />
+          </div>
         </Link>
 
         <div className="flex gap-2">
           <a href="https://twitter.com/pepseiclub" target="_blank">
+            <Image
+              width={32}
+              height={32}
+              alt={"logo"}
+              src={"/x.png"}
+              className="cursor-pointer rounded-full active:opacity-80"
+            />
+          </a>
 
-            
-          
-          <Image
-            width={32}
-            height={32}
-            alt={"logo"}
-            src={"/x.png"}
-            className="rounded-full cursor-pointer active:opacity-80"
-          />
-</a>
           <Image
             width={32}
             height={32}
             alt={"logo"}
             src={"/tglogo2.png"}
-            className="rounded-full cursor-pointer active:opacity-80"
+            className="cursor-pointer rounded-full active:opacity-80 opacity-60"
           />
         </div>
       </div>
 
-<Link href={'/roadmap'}>
-      <div className="flex flex-row gap-2 text-xl underline font-bold ">
-        <button className="active:text-black/80">Roadmap</button>
-      </div>
-</Link>
-
+      <Link href={"/roadmap"}>
+        <div className="flex flex-row gap-2 text-xl font-bold underline ">
+          <button className="active:text-black/80">Roadmap</button>
+        </div>
+      </Link>
     </header>
   );
 };
