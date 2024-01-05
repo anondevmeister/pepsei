478b4aef577bf75c7d954a4ce92d62feb59ab1c7
anondevmeister <degenprogrammer@gmail.com>
2024-01-05 18:47:39 +0100

    fix: buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,12 +16,16 @@ export default function HomePage() {
           className="rounded-lg"
         />
 
-        <div className="flex-col flex gap-4 py-4 w-full items-center">
-          <div className="flex items-center gap-4 w-2/5 md:w-1/5 justify-center py-1 px-2 rounded-md bg-black active:bg-black/80 cursor-pointer">
-            <button className="text-xl flex w-fit flex-row items-center justify-between gap-4">
-              Trade
-            </button>
-
+        <div className="flex w-full flex-col items-center gap-4 py-4">
+          <div className="flex w-2/5 cursor-pointer items-center justify-center gap-4 rounded-md bg-black px-2 py-1 active:bg-black/80 md:w-1/5">
+            <a
+              href="https://app.astroport.fi/swap?to=usei&from=sei1ugvzw3ge7le6s3wk7yg4nqvxx9njps0x8ekjtn6z7vg8wv9pkj3smapzd0"
+              target="_blank"
+            >
+              <button className="flex w-fit flex-row items-center justify-between gap-4 text-xl">
+                Trade
+              </button>
+            </a>
             <div>
               <Image
                 width={32}
@@ -30,28 +34,35 @@ export default function HomePage() {
                 src={"/astroport2.png"}
                 className="rounded-full"
               />
-          </div>
             </div>
-
-          
-            <div className="flex items-center gap-4 w-2/5 md:w-1/5 justify-center py-1 px-2 rounded-md bg-black active:bg-black/80 cursor-pointer">
-            <button className="text-xl flex w-fit flex-row items-center justify-between gap-4 curo">
-              Chart
-            </button>
-
-            <div>
-              <Image
-                width={32}
-                height={32}
-                alt={"logo"}
-                src={"/coinhall.png"}
-                className="rounded-full"
-              />
           </div>
+
+  
+            <div className="flex w-2/5 cursor-pointer items-center justify-center gap-4 rounded-md bg-black px-2 py-1 active:bg-black/80 md:w-1/5">
+         
+            <a
+            href="https://coinhall.org/sei/sei1uagqyzgw49n6jjwey2c44htwgtjgs4vvzt45cxsvswrg74z5z9jsce6pfp"
+            target="_blank"
+          >
+              <button className="curo flex w-fit flex-row items-center justify-between gap-4 text-xl">
+                Chart
+              </button>
+              </a>      
+              <div>
+                <Image
+                  width={32}
+                  height={32}
+                  alt={"logo"}
+                  src={"/coinhall.png"}
+                  className="rounded-full"
+                />
+        
+              </div>
+                      
             </div>
-
-
+            
         </div>
+        
       </div>
     </main>
   );
